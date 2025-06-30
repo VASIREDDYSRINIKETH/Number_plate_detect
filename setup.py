954be1451f92ce0f8751e95e72292f525b3c61c6
VASIREDDYSRINIKETH <sriniketh.2003.14@gmail.com>
2025-06-30 19:50:16 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'NumberPlateDetect',
+    version='0.0.0',
+    author='Vasireddy Sriniketh',
+    author_email='sriniketh.2003.14@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
